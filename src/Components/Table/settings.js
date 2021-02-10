d4f8e92caa1a2b5149170249a8fb802f4449bcf5
Aref Akbari <aref@pchas.ir>
2021-02-10 11:30:40 +0330

    setting

--- src/Components/Table/settings.js
+++ src/Components/Table/settings.js
@@ -1,18 +1,18 @@
 const settings = {
-    tableClass: '',
-    theadClass: '',
-    tbodyClass: '',
-    headerClass: '',
-    cellClass: 'cell',
-    filterInputClass: '',
-    filterPlaceholder: 'جستجو در لیست ...',
-    filterNoResults: 'هیچ داده ای یافت نشد',
-};
-
-export function mergeSettings(newSettings) {
-    for(const setting in newSettings) {
-        settings[setting] = newSettings[setting];
-    }
+	tableClass: '',
+	theadClass: '',
+	tbodyClass: '',
+	headerClass: '',
+	cellClass: 'cell',
+	filterInputClass: '',
+	filterPlaceholder: 'جستجو در لیست ...',
+	filterNoResults: 'هیچ داده ای یافت نشد'
 }
 
-export default settings;
+export function mergeSettings(newSettings) {
+	for (const setting in newSettings) {
+		settings[setting] = newSettings[setting]
+	}
+}
+
+export default settings
