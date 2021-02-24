7a935543a39d72983e6c60791c7123f74907574c
Aref Akbari <aref@pchas.ir>
2021-02-24 11:43:30 +0330

    inline-block

--- src/Components/Button/Button.vue
+++ src/Components/Button/Button.vue
@@ -1,84 +1,95 @@
 <template>
-<div class="inline-block">
-  <button :disabled="disabled" class="ty-flex ty-button ty-hoverable" :class="[...typeCss, type, size, disabled?'disabled':'']" :style="{width, height, borderRadius: radius, borderWidth: outlineWidth}" @click="handleClick">
-    <div class="ty-flex ty-gap-5 ty-justify-center">
-	  <i v-if="icon" :class="['my-auto', 'ty-icon', icon]"/>
-	  <slot></slot>
-    </div>
+  <!-- <div :class="['inline-block', 'ty-button', ...typeCss, type, size]" @click="handleClick"
+    :style="{width, height, borderRadius: radius, borderWidth: outlineWidth}"> -->
+  <button :disabled="disabled" :class="['inline-block', 'ty-button', ...typeCss, type, size, disabled?'disabled':'']"
+    @click="handleClick" 
+    @mousedown="(e) => $emit('mousedown', e)"
+    @mouseup="(e) => $emit('mouseup', e)"
+    @contextmenu="(e) => $emit('contextmenu', e)"
+    :style="{width, height, borderRadius: radius, borderWidth: outlineWidth}">
+      <i v-if="icon" :class="['inline-block','ty-icon', icon]" />
+      <div v-if="hasSlot" class="inline-block">
+        <slot></slot>
+      </div>
   </button>
-</div>
+  <!-- </div> -->
 </template>
 
 <script>
-export default {
-  name: 'TyButton',
+  export default {
+    name: 'TyButton',
+    inheritAttrs: false,
+    // *----------------------- P r o p s ----------------------------------------------------------
+    props: {
+      color: {
+        type: String,
+        default: 'primary'
+      },
+      outlineWidth: {
+        type: [String, Number],
+        default: '2px'
+      },
+      outlineColor: {
+        type: String,
+        default: null
+      },
+      radius: {
+        type: [String, Number],
+        default: '3px'
+      },
+      type: {
+        type: String,
+        default: 'simple'
+      },
+      size: {
+        type: String,
+        default: 'medium',
+      },
+      icon: {
+        type: String,
+        default: null
+      },
+      width: {
+        type: [Number, String],
+        required: false
+      },
+      height: {
+        type: [Number, String],
+        required: false
+      },
+      disabled: Boolean,
+    },
 
-  // *----------------------- P r o p s ----------------------------------------------------------
-  props: {
-	  color: {
-		  type: String,
-		  default: 'primary'
+    // *----------------------- D a t a -----------------------------------------------------------
+    data() {
+      return {}
     },
-    outlineWidth: {
-      type: [String, Number],
-      default: '2px'
-    },
-    outlineColor: {
-      type: String,
-      default: null
-    },
-    radius: {
-      type: [String, Number],
-      default: '3px'
-    },
-    type: {
-      type: String,
-      default: 'simple'
-    },
-    size: {
-      type: String,
-      default: 'medium',
-    },
-    icon: {
-      type: String,
-      default: null
-    },
-    width: {
-      type: [Number, String],
-      required: false
-    },
-    height: {
-      type: [Number, String],
-      required: false
-    },
-    disabled: Boolean,
-  },
 
-  // *----------------------- D a t a -----------------------------------------------------------
-  data() {
-    return {}
-  },
+    // *----------------------- C o m p u t e d ---------------------------------------------------
+    computed: {
+      typeCss() {
+        if (this.type == 'simple') return [`ty-bg-${this.color}`];
+        if (this.type == 'clear') return ['clear', `ty-color-${this.color}`];
+        if (this.type == 'outline') return ['outline', `ty-color-${this.color}`,
+          `ty-border-color-${this.outlineColor||this.color}`
+        ];
+      },
+      hasSlot() {
+        return !!this.$slots.default
+      },
+    },
 
-  // *----------------------- C o m p u t e d ---------------------------------------------------
-  computed: {
-    typeCss() {
-      if (this.type=='simple') return [`ty-bg-${this.color}`];
-      if (this.type=='clear') return ['clear', `ty-color-${this.color}`];
-      if (this.type=='outline') return ['outline', `ty-color-${this.color}`, `ty-border-color-${this.outlineColor||this.color}`];
-    }
-  },
+    // *----------------------- L i f e   c i r c l e ---------------------------------------------
+    created() {},
 
-  // *----------------------- L i f e   c i r c l e ---------------------------------------------
-  created() {},
+    // *----------------------- M e t h o d s -----------------------------------------------------
+    methods: {
+      handleClick(event) {
+        this.$emit('click', event);
+      },
+    },
 
-  // *----------------------- M e t h o d s -----------------------------------------------------
-  methods: {
-	  handleClick(event) {
-		  this.$emit('click', event);
-	  }
-  },
-
-  // *----------------------- W a t c h ---------------------------------------------------------
-  watch: {}
-}
+    // *----------------------- W a t c h ---------------------------------------------------------
+    watch: {}
+  }
 </script>
