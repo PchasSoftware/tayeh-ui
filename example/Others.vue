26df806a90937480fa3008230ceb70842d1362e6
Aref Akbari <aref@pchas.ir>
2021-06-28 18:26:59 +0430

    flex gap fallback

--- example/Others.vue
+++ example/Others.vue
@@ -139,6 +139,14 @@
         {{color}}
       </ty-card>
     </section>
+    <div class="ty-flex ty-gap-24">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+    </div>
 </div>
 </template>
 
