0e628eb7266ac5f9a59b446465d0d681c182df3f
Aref Akbari <aref@pchas.ir>
2021-11-17 12:34:06 +0330

    hide title

--- src/Components/Table/TableHeader.vue
+++ src/Components/Table/TableHeader.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="ty-flex ty-space-between mb-1">
 		<div class="ty-flex ty-flex-wrap">
-			<h4 :class="['my-auto ml-2 pl-2 ty-border-left','ty-color-'+titleColor]">{{title}}</h4>
+			<h4 v-if="showTitle" :class="['my-auto ml-2 pl-2 ty-border-left','ty-color-'+titleColor]">{{title}}</h4>
 			<ty-input v-model="content" width="60%" @input="handleInput" @change="handleChange" v-if="showSearch" class="ty-color-border"
 				icon="ty-icon-search" :clear="true" :placeholder="searchPlaceholder" />
 		</div>
@@ -27,6 +27,10 @@
 				default: 'primary'
 			},
 			showSearch: {
+				type: Boolean,
+				default: true
+			},
+			showTitle: {
 				type: Boolean,
 				default: true
 			},
